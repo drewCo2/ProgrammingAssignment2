727907ef8ea068994f24ddd60c9335022915fa19
drewCo2 <drew@As-MacBook-Pro.home>
2015-08-21 15:48:47 -0400

    Updated cache function so they work correctly

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# Creates a list suitable for caching a matrix, and its inverse.
+# There is no 'set' function as these items should be considered immutable.
 makeCacheMatrix <- function(x = matrix()) {
 
-    c<-NULL
-    list(mat=x, cache=c)
+  myVal = x
+  c<-NULL
+  get<-function() { myVal }
+  getCache<-function() { c }
+  setCache<-function(val) { c <<- val }
+  
+  list(value=get, inverse=getCache, setCache=setCache)
 }
 
 
-## Write a short comment describing this function
-
+# Given an appropriate list from 'makeCacheMatrix', this function will return the inverse of its input matrix.
+# The cache of the input list will be updated if needed.
 cacheSolve <- function(x, ...) {
 
   # check the cache for the inverse.
-  if (!is.na(x$cache))
+  i = x$inverse()
+  if (!is.null(i))
   {
-    res<-x$cachen
+    message("Getting cached value!")
+    res<-i
   }
   else
   {
-    res<-solve(x$mat, ...)
-    x$cache = res
+    # uncached, compute the inverse and cache it.
+    res<-solve(x$value(), ...)
+    x$setCache(res)
   }
-    # if it is cahced, use it,
-  
-  #if not..
-          ## Return a matrix that is the inverse of 'x'
 
   res
   
-  }
+}
